2c8a82d5b34ddeae8dbb44df544220f19b8ca58b
malopro <malovagilbert@gmail.com>
2023-03-25 15:28:30 -0700

    printf Task 0

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -13,43 +14,42 @@ int _printf(const char *format, ...)
 	int count = 0;
 	va_list args;
 
-	va_start(args, format);
+	while (*format != NULL)
+		va_start(args, format);
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
+		while (*format != '\0')
 		{
+			if (*format == '%')
+			{
+				++format;
+				if (*format == 'c')
+				{
+					char c = (char)va_arg(args, int);
+
+					_putchar(c);
+					++count;
+				}
+				else if (*format == 's')
+				{
+					char *s = va_arg(args, char *);
+
+					fputs(s, stdout);
+					count += strlen(s);
+				}
+				else if (*format == '%')
+				{
+					_putchar('%');
+					++count;
+				}
+			}
+			else
+			{
+				_putchar(*format);
+				++count;
+			}
 			++format;
-			if (*format == 'c')
-			{
-				char c = (char)va_arg(args, int);
-
-				_putchar(c);
-				++count;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				fputs(s, stdout);
-				count += strlen(s);
-			}
-			else if (*format == '%')
-			{
-				_putchar('%');
-				++count;
-			}
 		}
-		else
-		{
-			_putchar(*format);
-			++count;
-		}
-		++format;
-	}
-
-	va_end(args);
-	_putchar('\n');
+		va_end(args);
 	return (count);
 }
 
