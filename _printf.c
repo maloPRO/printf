f26081d7a647da77c017cdf8e91c2185b0cbbfbe
malopro <malovagilbert@gmail.com>
2023-03-25 14:52:25 -0700

    printf Task 0

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* _printf - produces output according to a format.
+* @format: character string
+* Return: the number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			++format;
+			if (*format == 'c')
+			{
+				char c = (char)va_arg(args, int);
+
+				_putchar(c);
+				++count;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				fputs(s, stdout);
+				count += strlen(s);
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				++count;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			++count;
+		}
+		++format;
+	}
+
+	va_end(args);
+	return (count);
+}
+
